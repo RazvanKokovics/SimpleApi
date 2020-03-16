c9bbe5f0c4a60eb19f310c5bc887232231dc71af
Razvan Kokovics <razvan.kokovics@algotech.solutions>
2020-03-16 13:23:43 +0200

    Working Connection.

---
+++ init.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users ( 
+    id SERIAL PRIMARY KEY, 
+    user_name VARCHAR(100) UNIQUE NOT NULL,
+    e_mail VARCHAR(100) UNIQUE NOT NULL,
+    first_name VARCHAR(100) UNIQUE NOT NULL,
+    last_name VARCHAR(100) UNIQUE NOT NULL,
+    password VARCHAR(100) NOT NULL
+);
+
+INSERT INTO users (id, user_name, e_mail, first_name, last_name, password)
+VALUES  (0, 'razvan', 'kokovics.razvan@gmail.com', 'Razvan', 'Kokovics', 'razvan');
