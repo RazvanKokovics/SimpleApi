ef82f9456b5af0cb326957a29030279913cc8264
Razvan Kokovics <razvan.kokovics@algotech.solutions>
2020-04-15 11:08:20 +0300

    New error logic

--- service/users.js
+++ service/users.js
@@ -1,52 +1,97 @@
-import { InexistentItem } from '../validators/errors';
+import { ValidationError, UniqueConstraintError } from 'sequelize';
+
+import {
+  InexistentItem,
+  CustomUniqueConstraintError,
+  CustomValidationError,
+  UnexpectedError,
+} from '../validators/errors';
 import userRepository from '../repository/users';
 import expressionRepository from '../repository/expression';
 import { hashPassword } from '../utils';
 
 class UserService {
-  fetchUsers() {
-    return userRepository.getUsers();
+  async fetchUsers() {
+    try {
+      return await userRepository.getUsers();
+    } catch (error) {
+      throw new UnexpectedError();
+    }
   }
 
   async insertUser(user) {
-    const { password } = user;
+    try {
+      const { password } = user;
 
-    const newPassword = await hashPassword(password);
+      const newPassword = await hashPassword(password);
 
-    const updatedUserData = {
-      ...user,
-      password: newPassword,
-      role: 2,
-    };
+      const updatedUserData = {
+        ...user,
+        password: newPassword,
+        role: 2,
+      };
 
-    return userRepository.addUser(updatedUserData);
+      return await userRepository.addUser(updatedUserData);
+    } catch (error) {
+      if (error instanceof ValidationError) {
+        throw new CustomValidationError(error);
+      } else if (error instanceof UniqueConstraintError) {
+        throw new CustomUniqueConstraintError(error.message);
+      }
+
+      throw new UnexpectedError();
+    }
   }
 
   async removeUser(userName) {
-    const user = await userRepository.getUserByUserName(userName);
+    try {
+      const user = await userRepository.getUserByUserName(userName);
 
-    if (!user) {
-      throw new InexistentItem('The user with this username does not exists.');
+      if (!user) {
+        throw new InexistentItem(
+          'The user with this username does not exists.',
+        );
+      }
+
+      await userRepository.deleteUser(userName);
+      await expressionRepository.deleteUserFromExpression(user.id);
+    } catch (error) {
+      if (error instanceof InexistentItem) {
+        throw error;
+      }
+
+      throw new UnexpectedError();
     }
-
-    await userRepository.deleteUser(userName);
-    await expressionRepository.deleteUserFromExpression(user.id);
   }
 
   async changeUser(user, userName) {
-    const { password } = user;
+    try {
+      const { password } = user;
 
-    const data = password
-      ? { ...user, password: await hashPassword(password), userName }
-      : { ...user, userName };
+      const data = password
+        ? { ...user, password: await hashPassword(password), userName }
+        : { ...user, userName };
 
-    const updated = await userRepository.updateUser(data);
+      const updated = await userRepository.updateUser(data);
 
-    if (!updated[0]) {
-      throw new InexistentItem('The user with this username does not exists.');
+      if (!updated[0]) {
+        throw new InexistentItem(
+          'The user with this username does not exists.',
+        );
+      }
+
+      return updated[1][0];
+    } catch (error) {
+      if (error instanceof InexistentItem) {
+        throw error;
+      } else if (error instanceof ValidationError) {
+        throw new CustomValidationError(error);
+      } else if (error instanceof UniqueConstraintError) {
+        throw new CustomUniqueConstraintError(error.message);
+      }
+
+      throw new UnexpectedError();
     }
-
-    return updated[1][0];
   }
 }
 
